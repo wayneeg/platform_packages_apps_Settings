da27072968efcf5a64e5a4452f2c9b7e57c5f2ea
maxwen <max.weninger@gmail.com>
2018-09-04 12:42:25 +0200

    Settings: add back SwipeToNotificationSuggestionActivity
    so it can be called from e.g device settings
    
    Change-Id: I3cf7aed1afa5fb21ece539bb514eb8b0465d9b77
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -179,4 +179,5 @@ public class Settings extends SettingsActivity {
 
     public static class DerpQuestSettingsActivity extends SettingsActivity { /* empty */ }
     public static class QuickSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class SwipeToNotificationSuggestionActivity extends SettingsActivity { /* empty */ }
 }
