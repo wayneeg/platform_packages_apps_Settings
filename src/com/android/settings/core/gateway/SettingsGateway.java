exthmui-aosp/android_packages_apps_Settings

Dark theme settings activity
Dark theme settings is now launchable as an activity

Test: launch activity android.settings.DARK_THEME_SETTINGS
Fixes: 145560830

Change-Id: I24b8d3211e9d6e7a5b1f226001882573ae7fb311

Former-commit-id: 82a0ea022ecd2bd97f7d9b28c9c8f814eec25237

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -74,6 +74,7 @@ import com.android.settings.deviceinfo.aboutphone.MyDeviceInfoFragment;
 import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionSettings;
 import com.android.settings.deviceinfo.legal.ModuleLicensesDashboard;
 import com.android.settings.display.NightDisplaySettings;
+import com.android.settings.display.darkmode.DarkModeSettingsFragment;
 import com.android.settings.dream.DreamSettings;
 import com.android.settings.enterprise.EnterprisePrivacySettings;
 import com.android.settings.fuelgauge.AdvancedPowerUsageDetail;
@@ -281,7 +282,8 @@ public class SettingsGateway {
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             BatterySaverScheduleSettings.class.getName(),
             MobileNetworkListFragment.class.getName(),
-            GlobalActionsPanelSettings.class.getName()
+            GlobalActionsPanelSettings.class.getName(),
+            DarkModeSettingsFragment.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

wayneeg/platform_packages_apps_Settings

Signed-off-by: Joey Huab <joey@evolution-x.org>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -75,6 +75,7 @@ import com.android.settings.deviceinfo.aboutphone.MyDeviceInfoFragment;
 import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionSettings;
 import com.android.settings.deviceinfo.legal.ModuleLicensesDashboard;
 import com.android.settings.display.NightDisplaySettings;
+import com.android.settings.display.darkmode.DarkModeSettingsFragment;
 import com.android.settings.dream.DreamSettings;
 import com.android.settings.enterprise.EnterprisePrivacySettings;
 import com.android.settings.fuelgauge.AdvancedPowerUsageDetail;
@@ -289,7 +290,8 @@ public class SettingsGateway {
             DerpQuestSettings.class.getName(),
             QuickSettings.class.getName(),
             // AEX additions
-            RunningServices.class.getName()
+            RunningServices.class.getName(),
+            DarkModeSettingsFragment.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {